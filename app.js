8e885b4c690ecce77a5d8ba2c4abcf30e2e1a3f2
Remy-Desire <airdesire38@gmail.com>
2021-09-06 12:30:19 +0200

    Animation Chargement + Finalisation du projet

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let allPokemon = [];
 let tableauFin = [];
 const searchInput = document.querySelector('.recherche-poke input');
-const listePoke = document.querySelector('.liste-poke')
+const listePoke = document.querySelector('.liste-poke');
+const chargement = document.querySelector('.loader');
 
 const types = {
     grass: '#78c850',
@@ -66,6 +67,7 @@ function fetchPokemonComplet(pokemon) {
                 }).slice(0,21);
 
                 createCard(tableauFin);
+                chargement.style.display = "none";
             }
         })
     })
